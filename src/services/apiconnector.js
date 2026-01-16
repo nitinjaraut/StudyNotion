08a92a644b2e58351919d81d3189ded1c1c25d91
nitin jaraut <nitinjaraut@nitins-MacBook-Air.local>
2026-01-16 21:43:01 +0530

    Fix CORS by removing global withCredentials

--- src/services/apiconnector.js
+++ src/services/apiconnector.js
@@ -1,26 +1,13 @@
-// import axios from "axios";
-// export const axiosInstance = axios.create({});
-// export const apiconnector = (method, url, bodyData, headers, params ) => {
-//     return axiosInstance ({
-//         method : `${method}`,
-//         url :  `${url}`,
-//         data : bodyData  ? bodyData : null,
-//         headers : headers ? headers : null,
-//         params : params ? params : null,
-//     });
-// }
 import axios from "axios";
 
-export const axiosInstance = axios.create({
-  withCredentials: true, // 🔥 REQUIRED
-});
+export const axiosInstance = axios.create({});
 
-export const apiconnector = (method, url, bodyData, headers, params) => {
+export const apiConnector = (method, url, bodyData, headers, params) => {
   return axiosInstance({
-    method: method,
-    url: url,
-    data: bodyData ?? null,
-    headers: headers ?? {},
-    params: params ?? {},
+    method: `${method}`,
+    url: `${url}`,
+    data: bodyData ? bodyData : null,
+    headers: headers ? headers : null,
+    params: params ? params : null,
   });
 };
